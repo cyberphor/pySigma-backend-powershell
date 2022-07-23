3c13411d566fbae6566fe8f133746580e6e11ea6
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-07-23 10:50:36 -0700

    Initial commit

---
+++ tests/test_backend_powershell.py
@@ -0,0 +1,159 @@
+import pytest
+from sigma.backends.powershell import PowerShellBackend
+
+@pytest.fixture
+def powershell_backend():
+    return PowerShellBackend()
+
+# TODO: implement tests for some basic queries and their expected results.
+def test_powershell_and_expression(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA: valueA
+                    fieldB: valueB
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_or_expression(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel1:
+                    fieldA: valueA
+                sel2:
+                    fieldB: valueB
+                condition: 1 of sel*
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_and_or_expression(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA:
+                        - valueA1
+                        - valueA2
+                    fieldB:
+                        - valueB1
+                        - valueB2
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_or_and_expression(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel1:
+                    fieldA: valueA1
+                    fieldB: valueB1
+                sel2:
+                    fieldA: valueA2
+                    fieldB: valueB2
+                condition: 1 of sel*
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_in_expression(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA:
+                        - valueA
+                        - valueB
+                        - valueC*
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_regex_query(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA|re: foo.*bar
+                    fieldB: foo
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_cidr_query(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    field|cidr: 192.168.0.0/16
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+def test_powershell_field_name_with_whitespace(powershell_backend : PowerShellBackend):
+    assert powershell_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    field name: value
+                condition: sel
+        """)
+    ) == ['<insert expected result here>']
+
+# TODO: implement tests for all backend features that don't belong to the base class defaults, e.g. features that were
+# implemented with custom code, deferred expressions etc.
+
+
+
+def test_powershell_format1_output(powershell_backend : PowerShellBackend):
+    """Test for output format format1."""
+    # TODO: implement a test for the output format
+    pass
+
+def test_powershell_format2_output(powershell_backend : PowerShellBackend):
+    """Test for output format format2."""
+    # TODO: implement a test for the output format
+    pass
+
---
+++ tests/test_pipelines_powershell.py
@@ -0,0 +1,5 @@
+import pytest
+from sigma.backends.powershell import PowerShellBackend
+from sigma.pipelines.powershell import # TODO: import pipeline functions
+
+# TODO: import tests for all implemented pipelines and contained transformations
