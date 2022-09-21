16be958290dbc79178a647b8481e05323ce9c1d2
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-09-20 21:17:24 -0700

    Update sigma2powershell.py

--- sigma2powershell.py
+++ sigma2powershell.py
@@ -7,6 +7,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--rule-file', type = str, help = 'path to Sigma rule file')
 args = parser.parse_args()
 
+EventParser = """
+filter Read-WinEvent {
+    $WinEvent = [ordered]@{} 
+    $XmlData = [xml]$_.ToXml()
+    $SystemData = $XmlData.Event.System
+    $SystemData | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name |
+    ForEach-Object {
+        $Field = $_
+        if ($Field -eq 'TimeCreated') {
+            $WinEvent.$Field = Get-Date -Format 'yyyy-MM-dd hh:mm:ss' $SystemData[$Field].SystemTime
+        } elseif ($SystemData[$Field].'#text') {
+            $WinEvent.$Field = $SystemData[$Field].'#text'
+        } else {
+            $SystemData[$Field] | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name |
+            ForEach-Object { 
+                $WinEvent.$Field = @{}
+                $WinEvent.$Field.$_ = $SystemData[$Field].$_
+            }
+        }
+    }
+    $XmlData.Event.EventData.Data | ForEach-Object { WinEvent.$($_.Name) = $_.'#text' }
+    return New-Object -TypeName PSObject -Property $WinEvent
+}
+"""
+
 def sigma2powershell(rulefile: str):
     rules = sigma.collection.SigmaCollection.load_ruleset([rulefile])
     pipeline = powershell_pipeline()
@@ -17,6 +42,7 @@ def sigma2powershell(rulefile: str):
 
 if __name__ == "__main__":
     if args.rule_file:
+        print(EventParser)
         sigma2powershell(args.rule_file)
     else:
         parser.print_help()
