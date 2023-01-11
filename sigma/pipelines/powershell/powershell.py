52a83e5d6f19bb6e7e446ff5e27a504d84b18ea0
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-01-11 08:59:04 -0800

    Debugging RuleFailureTransformation logic

--- sigma/pipelines/powershell/powershell.py
+++ sigma/pipelines/powershell/powershell.py
@@ -1,24 +1,27 @@
 from sigma.pipelines.common import logsource_windows, windows_logsource_mapping
-from sigma.processing.conditions import ExcludeFieldCondition, IncludeFieldCondition, LogsourceCondition
+from sigma.processing.conditions import LogsourceCondition
 from sigma.processing.pipeline import ProcessingPipeline, ProcessingItem
-from sigma.processing.transformations import AddFieldnamePrefixTransformation, RuleFailureTransformation
+from sigma.processing.transformations import AddConditionTransformation, AddFieldnamePrefixTransformation, RuleFailureTransformation
 
 def powershell_pipeline() -> ProcessingPipeline:
     return ProcessingPipeline(
         name = "PowerShell pipeline",
         items = [
             ProcessingItem(
-                transformation = AddFieldnamePrefixTransformation("$_.")
+                rule_condition_negation = True,
+                rule_conditions = [LogsourceCondition(product = "windows")],
+                transformation = RuleFailureTransformation("Product not supported.")
             )
         ] + [
             ProcessingItem(
-                rule_condition_linking = any,
                 rule_condition_negation = True,
-                rule_conditions = [
-                    LogsourceCondition(product = "windows"),
-                    LogsourceCondition(service = "security") # TODO" should check if service is a windows logsource
-                ],
-                transformation = RuleFailureTransformation("Rule not supported."),
+                rule_conditions = [LogsourceCondition(service = logsource)],
+                transformation = RuleFailureTransformation("Service not supported.")
+            )
+            for logsource, channel in windows_logsource_mapping.items()
+        ] + [
+            ProcessingItem(
+                transformation = AddFieldnamePrefixTransformation("$_.")
             )
         ]
     )
--- sigma2powershell.py
+++ sigma2powershell.py
@@ -3,16 +3,17 @@ from sigma.collection import SigmaCollection
 from sigma.pipelines.powershell import powershell_pipeline
 from sigma.backends.powershell import PowerShellBackend
 
-def Sigma2PowerShell(path):
+def Sigma2PowerShell(path: str, show_errors: bool):
     rules = SigmaCollection.load_ruleset(inputs = [path])
     pipeline = powershell_pipeline()
-    backend = PowerShellBackend(processing_pipeline = pipeline, collect_errors = True)
+    backend = PowerShellBackend(processing_pipeline = pipeline, collect_errors = show_errors)
     return backend.convert(rule_collection = rules, output_format = "default")
 
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", type = str, help = "path to Sigma rule(s)")
-    parser.add_argument("-o", type = str, help = "output format")
+    parser.add_argument("-p", "--path", type = str, help = "path to Sigma rule(s)")
+    parser.add_argument("-o", "--output", type = str, help = "output format")
+    parser.add_argument("--show-errors", default = True, action = "store_false", help = "show rule errors")
     args = parser.parse_args()
-    queries = Sigma2PowerShell(args.p)
+    queries = Sigma2PowerShell(args.path, args.show_errors)
     print("\n".join(queries))
