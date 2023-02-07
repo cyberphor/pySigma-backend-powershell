1aac9369c4b850d527fc86ab372ca06b76782671
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-06 22:07:31 -0800

    2023-02-06, 2207

--- tests/test_pipelines_powershell.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from sigma.pipelines.powershell import powershell_pipeline
-from sigma.backends.powershell import PowerShellBackend
-
-# TODO: import tests for all implemented pipelines and contained transformations
