78f8f110cf6423d76c477b71c0c870ac8e17595b
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-06-18 21:30:59 -0700

    Updated "get-sigma-rule.py" Python script

--- get_sigma_rules.py
+++ get_sigma_rules.py
@@ -1,7 +1,18 @@
-import os
+
+from shutil import move
+from urllib.request import urlretrieve
+from glob import glob
+from os import remove
+from zipfile import ZipFile
 
 def main():
-  os.system("git clone https://github.com/SigmaHQ/sigma rules")
+  urlretrieve("https://github.com/SigmaHQ/sigma/archive/refs/heads/master.zip", "tmp.zip")
+  with ZipFile("tmp.zip", 'r') as zf:
+    zf.extractall("tmp")
+  for rule_dir in glob("tmp/sigma-master/rule*"):
+    move(rule_dir, "rules")
+  remove("tmp")
+  remove("tmp.zip")
 
 if __name__ == "__main__":
   main()
